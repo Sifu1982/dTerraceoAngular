909f1d8c06e8909f91a72c36b889cbe442788fea
Pistachu <ygomezma@gmail.com>
2020-08-25 18:51:25 +0200

    Buscador por nombre funcional

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { TerrazasService } from '../terrazas.service';
-import { debounceTime } from 'rxjs/operators';
+import { Terraza } from '../models/terraza.model';
 
 @Component({
   selector: 'app-home',
@@ -10,19 +10,15 @@ import { debounceTime } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
 
-  fomularioBusquedaNombre: FormGroup;
   fomularioBusquedaAvanzada: FormGroup;
 
   arrBarrio: String[];
   arrNombreTerraza: String[];
+  arrTempterrazas: Terraza[];
+
+  keyword: string;
 
   constructor(private terrazasService: TerrazasService) {
-
-    this.fomularioBusquedaNombre = new FormGroup({
-      nombre: new FormControl('', [
-
-      ])
-    });
 
     this.fomularioBusquedaAvanzada = new FormGroup({
 
@@ -41,21 +37,28 @@ export class HomeComponent implements OnInit {
     this.arrBarrio = [''];
     this.arrNombreTerraza = [];
 
+    this.arrTempterrazas = [];
+    this.keyword = 'rotulo';
+
   }
 
   ngOnInit(): void {
 
     const arrayTerrazasCarousel = this.terrazasService.getArrayTerrazasCarousel();
 
+    this.arrTempterrazas = this.terrazasService.getArrayTerrazasCarousel();
+    console.log(this.arrTempterrazas);
+
     const arrTemp = arrayTerrazasCarousel.map(terraza => terraza.desc_barrio_local);
     this.arrBarrio = [...new Set(arrTemp)];
     this.arrNombreTerraza = arrayTerrazasCarousel.map(terraza => terraza.rotulo);
 
-    this.fomularioBusquedaNombre.controls.nombre.valueChanges.pipe(debounceTime(500)).subscribe(value => {
-      console.log(this.arrNombreTerraza.find(nombre => nombre.toLowerCase().includes(value.toLowerCase())));
-    });
+    // this.fomularioBusquedaNombre.controls.nombre.valueChanges.pipe(debounceTime(500)).subscribe(value => {
+    //   console.log(this.arrNombreTerraza.find(nombre => nombre.toLowerCase().includes(value.toLowerCase())));
+    // });
 
   }
+
 
   onChangeDistrito($event) {
 
@@ -65,11 +68,28 @@ export class HomeComponent implements OnInit {
 
   }
 
-  onSubmitBusquedaNombre() {
-
-  }
-
   onSubmitBusquedaAvanzada() {
     console.log(this.fomularioBusquedaAvanzada.value);
   }
+
+
+  selectEvent(item) {
+    console.log(item); //El item es el objeto tipo terraza entero que he seleccionado
+
+  }
+
+  async onChangeSearch(val: string) {
+    // this.arrTempterrazas = this.terrazasService.getArrayTerrazasCarousel();
+
+    console.log(val);
+
+    // this.arrTempterrazas = [new Terraza(3678, 1, 'CENTRO', 103, 'CORTES', 28014, '440914,59', '4474421,52', 1, 'Abierto', 'CIRCULO DE BELLAS ARTES DE MADRID', 93.24, 'CALLE', 'ALCALA', 'NUM', 42, 'Acera', '8:00:00', '1:30:00', '8:00:00', '1:00:00', '8:00:00', '1:30:00', '8:00:00', '1:00:00', 26, 91)];
+
+    this.arrTempterrazas = await this.terrazasService.buscarTerrazaPorNombre(val);
+  }
+
+  onFocused(e) {
+    // do something when input is focused
+  }
+
 }
