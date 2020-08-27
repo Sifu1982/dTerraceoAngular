b9513c0b9a9e63724a9b8d6d4a285cb8022a14c3
DavidMartinezTrigo <davidmtrigo@gmail.com>
2020-08-27 17:15:08 +0200

    Botón home desaparece de la ruta /home y aparece en las demás rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dTerraceoAngular';
+
+  desHome: Boolean;
+  constructor(private router: Router) {
+    this.desHome = true;
+  }
+  ngOnInit() {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (this.router.url === '/home') {
+          this.desHome = false;
+        } else {
+          this.desHome = true;
+        }
+      }
+    })
+  }
 }
